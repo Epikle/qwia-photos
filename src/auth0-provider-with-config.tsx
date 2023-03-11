77f92d5da6a2aafcc8abd268c5c49abc6d6ed42b
Epikle <98042337+Epikle@users.noreply.github.com>
2023-03-11 15:44:48 +0200

    Updated dependencies (#3)

--- src/auth0-provider-with-config.tsx
+++ src/auth0-provider-with-config.tsx
@@ -21,8 +21,10 @@ export const Auth0ProviderWithConfig = ({
     <Auth0Provider
       domain={domain}
       clientId={clientId}
-      redirectUri={redirectUri}
-      audience={audience}
+      authorizationParams={{
+        redirect_uri: redirectUri,
+        audience,
+      }}
     >
       {children}
     </Auth0Provider>
